d710409898ef27c0b9263fb60211f8b51c63b2ea
NishuGoel <nishu0505@gmail.com>
2019-04-28 00:03:23 +0530

    Add files via upload

---
+++ SecondaryRoutes/src/app/edit-next/edit-next.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-next',
+  templateUrl: './edit-next.component.html',
+  styleUrls: ['./edit-next.component.css']
+})
+export class EditNextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
