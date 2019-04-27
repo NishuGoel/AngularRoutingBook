9b7f58fdc0d49640d1b10e0112840b02917690aa
NishuGoel <nishu0505@gmail.com>
2019-04-28 01:16:49 +0530

    Add files via upload

--- RouteResolvers/src/app/home/home.component.ts
+++ RouteResolvers/src/app/home/home.component.ts
@@ -1,1 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { UserData } from '../user-data.service';
 
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+ constructor(){}
+ ngOnInit(){}
+
+}
