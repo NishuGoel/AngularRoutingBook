466770830856b388f02d23b9fba1e07bb8336dd3
NishuGoel <nishu0505@gmail.com>
2019-04-27 23:59:06 +0530

    Add files via upload

--- SecondaryRoutes/src/app/app-routing.module.ts
+++ SecondaryRoutes/src/app/app-routing.module.ts
@@ -1,1 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NextComponent } from './next/next.component';
+import { ThirdComponent } from './third/third.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditNextComponent } from './edit-next/edit-next.component';
 
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot([
+    {path: 'home', component: HomeComponent},
+      // {path: '', redirectTo: 'third', pathMatch: 'full'},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '**', component: PageNotFoundComponent},
+  ])],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
